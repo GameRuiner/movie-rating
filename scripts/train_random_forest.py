257c05cc308c138f43214e463bd35520f886f2bf
Marko Golovko <markgolovko@gmail.com>
2025-06-04 11:50:30 +0200

    drop timestamp

--- scripts/train_random_forest.py
+++ scripts/train_random_forest.py
@@ -11,11 +11,11 @@ ratings = pd.read_csv(os.path.join(EXTRACT_DIR, "ratings.csv"))
 movies_with_tags_features = pd.read_csv("movies_with_tags_features.csv") 
 data = pd.merge(ratings, movies_with_tags_features, on="movieId")
 
-X = data.drop(columns=["userId", "movieId", "title", "rating"])
+X = data.drop(columns=["userId", "movieId", "title", "rating", "timestamp"])
 y = data["rating"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-dump(model, "models_bin/random_forest.joblib")
+dump(model, "models_bin/random_forest_no_timestamp.joblib")
